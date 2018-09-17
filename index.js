fbfb41f06d6f5f61d3dcffddc3fbd8c093827a1a
Justin Lechner <jlech42@gmail.com>
2018-09-17 07:20:26 -0400

    spotify oath in progress

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express');
 
-const app = express();
+// passport and spotify strategy
+const passport = require('passport');
+const SpotifyStrategy = require('passport-spotify').Strategy;
+const keys = require('./config/dev');
+
+const app = express(); // create express instance
+
+//create new instance of spotify strategy
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: keys.spotifyClientID,
+      clientSecret: keys.spotifyClientSecret,
+      callbackURL: '/auth/spotify/callback/'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
+
+app.get(
+  '/auth/spotify',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private']
+  })
+);
 
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
