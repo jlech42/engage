065f2c3a0e67c7fbb72819a9bfa964b1af62f1dd
Justin Lechner <jlech42@gmail.com>
2018-10-13 13:23:50 -0400

    static handling

--- index.js
+++ index.js
@@ -32,5 +32,16 @@ app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Express serve up prod assets
+  app.use(express.static('client/build'));
+
+  // Express will serve index.html if route not recognized
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
